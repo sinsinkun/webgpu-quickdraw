f15486ac3014b91cc2d2e2b5ac9752495e9b0392
Chang Xiao <chang.x1994@gmail.com>
2024-05-13 20:22:56 -0400

    Restored base url

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '' : '/vite-webgpu',
+export default defineConfig({
+  base: '/vite-webgpu',
   build: {
     outDir: 'build',
   },
   server: {
     port: 3001
   },
-}));
+});
