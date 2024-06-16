cbd35d61805a56705bd7ff0ff4a8e977d6200696
Chang Xiao <chang.x1994@gmail.com>
2024-06-16 11:22:48 -0400

    re-ordered primitives

--- src/primitives.ts
+++ src/primitives.ts
@@ -10,60 +10,7 @@ class Primitives {
     const normals: Array<[number, number, number]> = [];
     return { vertices, uvs, normals }
   }
-  static cube(width:number, height?:number, depth?:number): Shape {
-    const w = width/2;
-    const h = height ? height/2 : width/2;
-    const d = depth ? depth/2 : width/2;
-    const vertices: Array<[number, number, number]> = [
-      // face front
-      [w,-h,d],[w,h,d],[-w,-h,d],
-      [-w,h,d],[-w,-h,d],[w,h,d],
-      // face back
-      [-w,-h,-d],[-w,h,-d],[w,-h,-d],
-      [w,h,-d],[w,-h,-d],[-w,h,-d],
-      // face left
-      [-w,-h,d],[-w,h,d],[-w,-h,-d],
-      [-w,h,-d],[-w,-h,-d],[-w,h,d],
-      // face right
-      [w,-h,-d],[w,h,-d],[w,-h,d],
-      [w,h,d],[w,-h,d],[w,h,-d],
-      // face up
-      [w,-h,-d],[w,-h,d],[-w,-h,-d],
-      [-w,-h,d],[-w,-h,-d],[w,-h,d],
-      // face down
-      [w,h,d],[w,h,-d],[-w,h,d],
-      [-w,h,-d],[-w,h,d],[w,h,-d],
-    ];
-    const uvs: Array<[number, number]> = [
-      // face front
-      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
-      // face back
-      [0,1],[0,0],[1,1],[1,0],[1,1],[0,0],
-      // face left
-      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
-      // face right
-      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
-      // face up
-      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
-      // face down
-      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
-    ];
-    const normals: Array<[number, number, number]> = [
-      // face front
-      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
-      // face back
-      [0,0,-1],[0,0,-1],[0,0,-1],[0,0,-1],[0,0,-1],[0,0,-1],
-      // face left
-      [-1,0,0],[-1,0,0],[-1,0,0],[-1,0,0],[-1,0,0],[-1,0,0],
-      // face right
-      [1,0,0],[1,0,0],[1,0,0],[1,0,0],[1,0,0],[1,0,0],
-      // face up
-      [0,1,0],[0,1,0],[0,1,0],[0,1,0],[0,1,0],[0,1,0],
-      // face down
-      [0,-1,0],[0,-1,0],[0,-1,0],[0,-1,0],[0,-1,0],[0,-1,0],
-    ];
-    return { vertices, uvs, normals }
-  }
+  // 2d primitives
   static rect(width:number, height:number, zIndex:number = 0): Shape {
     const w = width / 2;
     const h = height / 2;
@@ -145,6 +92,61 @@ class Primitives {
     index.push(vertices.length-1, 0, 1);
 
     return { vertices, uvs, normals, index };
+  }
+  // 3d primitives
+  static cube(width:number, height?:number, depth?:number): Shape {
+    const w = width/2;
+    const h = height ? height/2 : width/2;
+    const d = depth ? depth/2 : width/2;
+    const vertices: Array<[number, number, number]> = [
+      // face front
+      [w,-h,d],[w,h,d],[-w,-h,d],
+      [-w,h,d],[-w,-h,d],[w,h,d],
+      // face back
+      [-w,-h,-d],[-w,h,-d],[w,-h,-d],
+      [w,h,-d],[w,-h,-d],[-w,h,-d],
+      // face left
+      [-w,-h,d],[-w,h,d],[-w,-h,-d],
+      [-w,h,-d],[-w,-h,-d],[-w,h,d],
+      // face right
+      [w,-h,-d],[w,h,-d],[w,-h,d],
+      [w,h,d],[w,-h,d],[w,h,-d],
+      // face up
+      [w,-h,-d],[w,-h,d],[-w,-h,-d],
+      [-w,-h,d],[-w,-h,-d],[w,-h,d],
+      // face down
+      [w,h,d],[w,h,-d],[-w,h,d],
+      [-w,h,-d],[-w,h,d],[w,h,-d],
+    ];
+    const uvs: Array<[number, number]> = [
+      // face front
+      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
+      // face back
+      [0,1],[0,0],[1,1],[1,0],[1,1],[0,0],
+      // face left
+      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
+      // face right
+      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
+      // face up
+      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
+      // face down
+      [1,0],[1,1],[0,0],[0,1],[0,0],[1,1],
+    ];
+    const normals: Array<[number, number, number]> = [
+      // face front
+      [0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],[0,0,1],
+      // face back
+      [0,0,-1],[0,0,-1],[0,0,-1],[0,0,-1],[0,0,-1],[0,0,-1],
+      // face left
+      [-1,0,0],[-1,0,0],[-1,0,0],[-1,0,0],[-1,0,0],[-1,0,0],
+      // face right
+      [1,0,0],[1,0,0],[1,0,0],[1,0,0],[1,0,0],[1,0,0],
+      // face up
+      [0,1,0],[0,1,0],[0,1,0],[0,1,0],[0,1,0],[0,1,0],
+      // face down
+      [0,-1,0],[0,-1,0],[0,-1,0],[0,-1,0],[0,-1,0],[0,-1,0],
+    ];
+    return { vertices, uvs, normals }
   }
   static cylinder(radius:number, height:number, sides: number): Shape {
     if (sides < 2) throw new Error("Sides count must be greater than 2");
